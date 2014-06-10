0e2a1d0ce9576d39c80fdf94a766d9bfdbcbd302
rundis <mrundberget@hotmail.com>
2014-06-11 01:15:47 +0200

    Run with utf-8 default for groovy server

--- run-server.sh
+++ run-server.sh
@@ -9,4 +9,4 @@ GROOVY_BIN=`which groovy`
 GROOVY_BIN_FOLDER=`dirname $GROOVY_BIN`
 GROOVY_ALL_JAR=`find "$GROOVY_BIN_FOLDER/../embeddable" -type f ! -name "*-indy.jar" | head -n 1`
 
-java -cp "$GROOVY_ALL_JAR:lib/ltgroovy.jar" lt.groovy.LTServer $1 $2 "$3"
+java -Dfile.encoding=UTF-8 -cp "$GROOVY_ALL_JAR:lib/ltgroovy.jar" lt.groovy.LTServer $1 $2 "$3"
